e342840910c99f8012f27e9d83e59f2feaaff734
Oleg Gribovsky <gribovsky@gmail.com>
2020-10-29 21:46:52 +0300

    commit from main

--- 201029-testGIT/ViewController.swift
+++ 201029-testGIT/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
        print("123")
-  //      print("456")
+       print("456")
+        print("678")
     }
 
 
