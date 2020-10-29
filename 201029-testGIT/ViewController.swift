db8da6d72376dc5f4f260ddc9ca202d8c2bf9fd9
Oleg Gribovsky <gribovsky@gmail.com>
2020-10-29 20:11:46 +0300

    Initial commit

---
+++ 201029-testGIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  201029-testGIT
+//
+//  Created by Oleg Gribovsky on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+
+
+}
+
